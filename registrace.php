6fcefb0bd0a67495aae992e3bbcabb5686e1d995
zajanaza <janazadrazilova@seznam.cz>
2023-08-21 11:18:38 +0200

    druhy-funkcni zapis do DB uzivatelu z formulare pres dblogin

--- registrace.php
+++ registrace.php
@@ -8,9 +8,9 @@
 <body>
   <h1>Registrace</h1>
   <?php
-  include "menu.php";
+  include "menu.php";  
   ?>
-  <form action="registrace.php" method="POST">
+  <form action="registraceVlozeniDat.php" method="POST">
     <div>
       <label for="input1">Jméno: </label>
       <input id="input1" type="text" name="jmeno">
@@ -30,27 +30,6 @@
     <div>
       <input type="submit" value="Registrovat">
     </div>
-  </form>
-  <?php
-  if (!($con = mysqli_connect("localhost","uzivatel","heslo","registr")))
-  {
-    die("Nelze se připojit k databázovému serveru!</body></html>");
-  }
-  mysqli_query($con,"SET NAMES 'utf8'");
-  if (mysqli_query($con,
-      "INSERT INTO uzivatele(jmeno, prijmeni, prihl_jmeno, heslo) VALUES('" .
-      addslashes($_POST["jmeno"]) . "', '" .
-      addslashes($_POST["prijmeni"]) . "', '" .
-      addslashes($_POST["prihl_jmeno"]) . "', '" .
-      addslashes($_POST["heslo"]) . "')"))
-  {
-    echo "Úspěšně vloženo.";
-  }
-  else
-  {
-    echo "Nelze provést dotaz. " . mysqli_error($con);
-  }
-  mysqli_close($con); 
-  ?>
+  </form>  
 </body>
 </html>
